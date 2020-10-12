c0cfa49a091fb08903feaecfa0006e52ac55223c
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-12 11:24:39 -0500

    added blog posts and url component

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,21 +1,36 @@
 import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 
-const Blog = (props) => (
-  <>
-    <h1>This is the blog</h1>
-    <article>
-      <h2>Blog post #1</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </article>
-    <article>
-      <h2>Blog post #2</h2>
-      <p>Maecenas consequat sagittis orci quis accumsan.</p>
-    </article>
-    <article>
-      <h2>Blog post #3</h2>
-      <p>Curabitur fringilla, risus dignissim egestas elementum.</p>
-    </article>
-  </>
-);
+const blogPosts = [
+  {
+    title: "Blog post #1",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    title: "Blog post #2",
+    content: "Maecenas consequat sagittis orci quis accumsan.",
+  },
+  {
+    title: "Blog post #3",
+    content:
+      "Curabitur fringilla, risus dignissim egestas elementum, leo sapien pretium est, quis imperdiet eros orci et ligula.",
+  },
+];
+
+const Blog = (props) => {
+  const { url } = useRouteMatch();
+  return (
+    <>
+      <h1>This is the blog</h1>
+      <nav>
+        {blogPosts.map((post, index) => (
+          <Link to={`${url}${index}`} key={index}>
+            {post.title}
+          </Link>
+        ))}
+      </nav>
+    </>
+  );
+};
 
 export default Blog;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Post = props => {
+    const { id } = useParams();
+    const post = props.post[id];
+
     return (
         <article>
-            <h1>Post Title</h1>
-            <h1>Post Text</h1>
+            <h1>{post.title}</h1>
+            <h1>{post.content}</h1>
         </article>
     )
 }
